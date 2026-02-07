491f2720d4f1e4b207bffc10d13192f7554ba360
Gaurav Wararkar <gauravwararkar143@gmail.com>
2026-02-07 21:32:37 +0530

    Practical 4 Lab Assignments

---
+++ Practical4/Assignment1(a).py
@@ -0,0 +1,14 @@
+#Lab Assignment-1
+
+"""
+Perform the following operations using Numpy
+
+a) Construct a Python program using NumPy to generate a 4x4 identity matrix.
+"""
+
+import numpy as np
+
+identity_matrix = np.identity(4)
+
+print("The 4x4 Identity Matrix:")
+print(identity_matrix)
