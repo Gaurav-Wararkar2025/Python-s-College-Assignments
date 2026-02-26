8f9370d02860bc7801492a499354c6bd7dacf566
Gaurav Wararkar <gauravwararkar143@gmail.com>
2026-02-26 23:56:46 +0530

    Practical 7 Lab Assignments

---
+++ Practical7/Assignment1.py
@@ -0,0 +1,15 @@
+#Lab Assignment-1
+
+"""
+Create a Menu Driven application "CAL.C" by implemnting different functions for the following basic operations:
+
+a) Addition
+
+b) Subtraction
+
+c) Multiplication
+
+d) Division
+
+e) Modulus
+"""
